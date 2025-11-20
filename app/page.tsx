d5705d7ab05eb95c87b6dde0941b18e15d5fbc71
memeonline <pablo.campu@gmail.com>
2025-11-20 14:14:28 -0600

    calendarly fix

--- app/page.tsx
+++ app/page.tsx
@@ -623,7 +623,7 @@ export default function ComingSoon() {
           {/* Book Consultation Button */}
           <motion.div
             className="button-container"
-            initial={{ opacity: 1, y: 0 }}
+            initial={{ opacity: 0, y: 10 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.6, delay: 0.4 }}
           >
