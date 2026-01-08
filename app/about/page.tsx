d4b3d1a4a6c63a34f42f6d968a068464fa2aebb8
memeonline <pablo.campu@gmail.com>
2026-01-07 18:26:10 -0600

    br fix

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -341,7 +341,6 @@ export default function About() {
               <p className="about-section-2-bio">
                 Danielle Robles is an LA-native creative director, copywriter, and content strategist with over 13 years of experience shaping narratives for some of the world&apos;s most recognizable brands. Coming from a multicultural background, she approaches every story with curiosity––about culture, identity, and the ways creativity shapes how we see the world.
                 <br />
-                <br />
                 From developing glossy eBay campaigns to capturing boots-on-the-ground content for New York Fashion Week, she believes that the stories worth telling are the ones that bleed authenticity. They linger in our minds long after we scroll away. They remind us we&apos;re human.
               </p>
             </div>
